515fd85c787a11c10778c4f927390a24d1c7eb98
Jitesh Pramodray <jiteshprm@gmail.com>
2021-04-13 21:45:01 +0100

    start of the ram logger

--- ram_logger/ram_logger.py
+++ ram_logger/ram_logger.py
@@ -14,6 +14,7 @@ TOPIC = 'sensors/#'
 DATABASE_FILE = '/mnt/ramdisk/mqtt_ramdisk.db'
 
 table_name_cache = {""}
+timestamp_msg = 0
 
 
 def current_milli_time():
@@ -24,31 +25,35 @@ def current_milli_time_to_human(millisecs_since_epoch):
     return datetime.datetime.fromtimestamp(float(millisecs_since_epoch)/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
 
 
+def print_with_msg_timestamp(msg):
+    print (current_milli_time_to_human(timestamp_msg) + msg)
+
+
 def on_connect(mqtt_client, user_data, flags, conn_result):
     mqtt_client.subscribe(TOPIC)
 
 
 def check_if_table_exists_in_cache(table_name):
     if table_name in table_name_cache:
-        print ("check_if_table_exists_in_cache - Table_name:" + table_name + " in cache")
+        print_with_msg_timestamp ("check_if_table_exists_in_cache - Table_name:" + table_name + " in cache")
         return True
     else:
-        print ("check_if_table_exists_in_cache - Table_name:" + table_name + " NOT in cache")
+        print_with_msg_timestamp ("check_if_table_exists_in_cache - Table_name:" + table_name + " NOT in cache")
         return False
 
 
 def check_if_table_exists_in_db(table_name, user_data):
-    print ("check_if_table_exists_in_db - Table_name:" + table_name)
+    print_with_msg_timestamp ("check_if_table_exists_in_db - Table_name:" + table_name)
     table_exists_in_db = False
     db_conn = user_data['db_conn']
     sql = """
             SELECT name FROM sqlite_master WHERE type='table' and name='%s';
                         """ % table_name
     cursor = db_conn.cursor()
-    print ("check_if_table_exists_in_db - Executing: " + sql)
+    print_with_msg_timestamp ("check_if_table_exists_in_db - Executing: " + sql)
     cursor.execute(sql)
     row_count = len(cursor.fetchall())
-    print ("check_if_table_exists_in_db - Found Number of Tables: " + str(row_count))
+    print_with_msg_timestamp ("check_if_table_exists_in_db - Found Number of Tables: " + str(row_count))
     if row_count > 0:
         table_name_cache.add(table_name)
         table_exists_in_db = True
@@ -57,18 +62,20 @@ def check_if_table_exists_in_db(table_name, user_data):
 
 
 def create_table_if_not_exists(table_name, user_data):
-    print ("create_table_if_not_exists - table_name:" + table_name)
+    print_with_msg_timestamp ("create_table_if_not_exists - table_name:" + table_name)
     db_conn = user_data['db_conn']
     sql = """
     CREATE TABLE IF NOT EXISTS """ + table_name + """ (
-        timestamp_raw INTEGER PRIMARY KEY NOT NULL,
-        timestamp_str TEXT NOT NULL,
+        timestamp_sensor_raw INTEGER PRIMARY KEY NOT NULL,
+        timestamp_sensor_str TEXT NOT NULL,
+        timestamp_msg_raw INTEGER NOT NULL,
+        timestamp_msg_str TEXT NOT NULL        
         value_raw INTEGER NOT NULL,
         value_str TEXT NOT NULL
     )
     """
     cursor = db_conn.cursor()
-    print ("create_table_if_not_exists - Executing: " + sql)
+    print_with_msg_timestamp ("create_table_if_not_exists - Executing: " + sql)
     cursor.execute(sql)
     cursor.close()
 
@@ -80,24 +87,27 @@ def check_if_table_exists_or_else_create(table_name, user_data):
 
 
 def on_message(mqtt_client, user_data, message):
-    print ("on_message - received mqtt_client:" + str(mqtt_client) + "user_data: " + str(user_data) + "message: " + str(message))
+    timestamp_msg = current_milli_time()
+    print_with_msg_timestamp ("on_message - received message: " + str(message.payload) + " topic: " + str(message.topic))
     payload = message.payload.decode('utf-8')
     table_name = message.topic.split("/")[1]
-    print ("on_message - table_name:" + table_name)
+    print_with_msg_timestamp ("on_message - table_name:" + table_name)
     check_if_table_exists_or_else_create(table_name, user_data)
     db_conn = user_data['db_conn']
-    sql = 'INSERT INTO ' + table_name + '(timestamp_raw, timestamp_str, value_raw, value_str) VALUES (?, ?, ?, ?)'
-    print ("on_message - Executing: " + sql)
+    sql = 'INSERT INTO ' + table_name + '(timestamp_sensor_raw, timestamp_sensor_str, timestamp_msg_raw, timestamp_msg_str, value_raw, value_str) VALUES (?, ?, ?, ?, ?, ?)'
     cursor = db_conn.cursor()
     payload_processed = payload.split(",")
-    timestamp_raw = payload_processed[0]
-    timestamp_str = current_milli_time_to_human(timestamp_raw)
+    timestamp_sensor_raw = payload_processed[0]
+    timestamp_sensor_str = current_milli_time_to_human(timestamp_raw)
     value_raw = payload_processed[1]
     value_str = payload_processed[2]
-    cursor.execute(sql, (timestamp_raw, timestamp_str, value_raw, value_str))
+    timestamp_msg_raw = current_milli_time
+    timestamp_msg_str = current_milli_time_to_human(timestamp_msg_raw)
+    print_with_msg_timestamp ("on_message - Executing: " + sql + "%d,%s,%d,%s,%d,%s" % (timestamp_sensor_raw, timestamp_sensor_str, timestamp_msg_raw, timestamp_msg_str, value_raw, value_str))
+    cursor.execute(sql, (timestamp_sensor_raw, timestamp_sensor_str, timestamp_msg_raw, timestamp_msg_str, value_raw, value_str))
     db_conn.commit()
     cursor.close()
-    print ("on_message - Finished processing message!")
+    print_with_msg_timestamp ("on_message - Finished processing message, waiting for next message...")
 
 
 def main():
