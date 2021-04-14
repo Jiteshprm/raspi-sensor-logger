9bda19ca6010422be271f3b171620beefbf00adf
Jitesh Pramodray <jiteshprm@gmail.com>
2021-04-14 23:01:58 +0100

    persistent logger code

--- persistent_logger/persistent_logger.py
+++ persistent_logger/persistent_logger.py
@@ -1,5 +1,3 @@
-#https://lindevs.com/save-mqtt-data-to-sqlite-database-using-python/
-import paho.mqtt.client as mqtt
 import sqlite3
 import time
 import datetime
@@ -20,29 +18,28 @@ def print_with_msg_timestamp(msg):
     print ("[" + current_milli_time_to_human(current_milli_time()) + "]: " + msg)
 
 
-def main():
-    print ("main - Starting")
-    print_with_msg_timestamp ("-----------------START------------------")
-    db_conn = sqlite3.connect(DATABASE_FILE_PERSISTENT)
+def attach_ramdisk_db(db_conn):
     sql = """
     ATTACH DATABASE '%s' AS mqtt_ramdisk;
     """ % DATABASE_FILE
     cursor = db_conn.cursor()
-    print_with_msg_timestamp ("main - Executing: " + sql)
+    print_with_msg_timestamp("main - Executing: " + sql)
     cursor.execute(sql)
+    cursor.close()
 
+
+def copy_ramdisk_to_persistent_db(db_conn, t_name):
     sql = """
-    SELECT name FROM mqtt_ramdisk.sqlite_master WHERE type='table';
-    """
+            insert or ignore into main.%s select * from mqtt_ramdisk.%s;
+            """ % (t_name, t_name)
     cursor = db_conn.cursor()
-    print_with_msg_timestamp ("main - Executing: " + sql)
+    print_with_msg_timestamp("main - Executing: " + sql)
     cursor.execute(sql)
-    alltables = cursor.fetchall()
-    print_with_msg_timestamp ("main - found %s tables: " % len(alltables))
-    for t in alltables:
-        t_name = t[0]
-        print_with_msg_timestamp ("main - Copying table: " + t_name)
-        sql = """
+    cursor.close()
+
+
+def create_table_in_persistent_db_if_not_exists(db_conn, t_name):
+    sql = """
             CREATE TABLE IF NOT EXISTS main.%s (
             timestamp_sensor_raw INTEGER PRIMARY KEY NOT NULL,
             timestamp_sensor_str TEXT NOT NULL,
@@ -51,17 +48,37 @@ def main():
             value_raw INTEGER NOT NULL,
             value_str TEXT NOT NULL);
         """ % t_name
-        cursor = db_conn.cursor()
-        print_with_msg_timestamp ("main - Executing: " + sql)
-        cursor.execute(sql)
-        sql = """
-            insert or ignore into main.%s select * from mqtt_ramdisk.%s;
-            """ % (t_name, t_name)
-        cursor = db_conn.cursor()
-        print_with_msg_timestamp ("main - Executing: " + sql)
-        cursor.execute(sql)
-        db_conn.commit()
+    cursor = db_conn.cursor()
+    print_with_msg_timestamp("main - Executing: " + sql)
+    cursor.execute(sql)
     cursor.close()
+
+
+def get_all_tables_from_ramdisk(db_conn):
+    sql = """
+    SELECT name FROM mqtt_ramdisk.sqlite_master WHERE type='table';
+    """
+    cursor = db_conn.cursor()
+    print_with_msg_timestamp("main - Executing: " + sql)
+    cursor.execute(sql)
+    alltables = cursor.fetchall()
+    return alltables
+
+
+def main():
+    print ("main - Starting")
+    print_with_msg_timestamp ("-----------------START------------------")
+    db_conn = sqlite3.connect(DATABASE_FILE_PERSISTENT)
+    attach_ramdisk_db(db_conn)
+
+    alltables = get_all_tables_from_ramdisk(db_conn)
+    print_with_msg_timestamp ("main - found %s tables: " % len(alltables))
+    for t in alltables:
+        t_name = t[0]
+        print_with_msg_timestamp("main - Copying table: " + t_name)
+        create_table_in_persistent_db_if_not_exists(db_conn, t_name)
+        copy_ramdisk_to_persistent_db(db_conn, t_name)
+        db_conn.commit()
     print_with_msg_timestamp ("-----------------FINISH------------------")
 
 main()
