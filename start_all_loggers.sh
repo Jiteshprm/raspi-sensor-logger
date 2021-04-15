19374ce3639e18be85ee32676c9845d3a8ff0b12
Jitesh Pramodray <jiteshprm@gmail.com>
2021-04-16 00:03:38 +0100

    Add start and stop services scripts

---
+++ start_all_loggers.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+#Must be run as sudo
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo!"
+  exit
+fi
+
+set -x
+systemctl daemon-reload
+systemctl enable ram_logger.service
+systemctl start ram_logger.service
+
+systemctl enable bh1750.service
+systemctl start bh1750.service
+systemctl enable bme280.service
+systemctl start bme280.service
+systemctl enable ds18s20.service
+systemctl start ds18s20.service
+systemctl enable persistent_logger.service
+systemctl start persistent_logger.service
+
+
+systemctl status ram_logger.service
+systemctl status bme280.service
+systemctl status ds18s20.service
+systemctl status bh1750.service
+systemctl status persistent_logger.service
---
+++ stop_all_loggers.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+#Must be run as sudo
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo!"
+  exit
+fi
+
+set -x
+systemctl stop ram_logger.service
+systemctl stop bme280.service
+systemctl stop ds18s20.service
+systemctl stop bh1750.service
+systemctl stop persistent_logger.service
