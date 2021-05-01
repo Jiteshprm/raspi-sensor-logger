62cf33877002e6c5c1a74adb59c8f5810dd6fb01
Jitesh Pramodray <jiteshprm@gmail.com>
2021-05-01 18:33:43 +0100

    Add ramdisk creation check before starting ram_logger

--- ram_logger/ram_logger.sh
+++ ram_logger/ram_logger.sh
@@ -5,4 +5,6 @@ mkdir /mnt/ramdisk
 mount -osize=30m tmpfs /mnt/ramdisk -t tmpfs
 chmod 777 /mnt/ramdisk
 sqlite3 /mnt/ramdisk/mqtt_ramdisk.db "VACUUM;PRAGMA auto_vacuum = 1;"
+chown birdofprey:birdofprey /mnt/ramdisk/mqtt_ramdisk.db
+chmod -R 777 /mnt/ramdisk
 echo "Directory /mnt/ramdisk created."
