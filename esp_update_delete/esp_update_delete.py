fa1d3b03b0785f15169f90f6c6db2e6b1069f6b4
Jitesh Pramodray <jiteshprm@gmail.com>
2021-05-03 02:08:44 +0100

    Add service to delete ESP Sketch update file esp_update_delete

--- esp_update_delete/esp_update_delete.py
+++ esp_update_delete/esp_update_delete.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3 -u
 from http.server import BaseHTTPRequestHandler,HTTPServer
 from subprocess import call
+import os
 
 # https://stackoverflow.com/questions/60757496/how-to-run-bash-script-from-nginx
 
 PORT_NUMBER = 8080
 autopull = '/opt/raspi-sensor-logger/esp_update_delete/esp_update_delete.sh'
 command = [autopull]
+sketch_update_file_path='/home/birdofprey/esp8266/OutdoorWeatherStation.ino.esp8285.bin'
 
 
 # This class will handles any incoming request from
@@ -15,20 +17,30 @@ class myHandler(BaseHTTPRequestHandler):
 
     # Handler for the GET requests
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        # Send the html message
-        self.wfile.write(("running {}".format(autopull)).encode())
-        call(command)
-        return
+        try:
+            if self.path.endswith(".bin"):
+                file_size = os.stat(sketch_update_file_path).st_size
+                f = open(sketch_update_file_path, 'rb')
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/plain')
+                self.send_header('Content-Length', file_size)
+                self.end_headers()
+                # Send the html message
+                self.wfile.write(f.read())
+                f.close()
+                call(command)
+                return
+            else:
+                self.send_error(404, 'File Not Found: %s' % self.path)
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
 
 
 try:
     # Create a web server and define the handler to manage the
     # incoming request
     server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print ('Started httpserver on port ' , PORT_NUMBER)
+    print ('Started http server on port ' , PORT_NUMBER)
 
     # Wait forever for incoming http requests
     server.serve_forever()
