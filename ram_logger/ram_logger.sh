9b19b147537908981558e8bd00dedcb1159db6e8
Jitesh Pramodray <jiteshprm@gmail.com>
2021-05-11 23:43:23 +0100

    Refactor Ram Logger startup to show up exceptions in journald

--- ram_logger/ram_logger.sh
+++ ram_logger/ram_logger.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf /mnt/ramdisk
-mkdir /mnt/ramdisk
-mount -osize=30m tmpfs /mnt/ramdisk -t tmpfs
-chmod 777 /mnt/ramdisk
-sqlite3 /mnt/ramdisk/mqtt_ramdisk.db "VACUUM;PRAGMA auto_vacuum = 1;"
-chown birdofprey:birdofprey /mnt/ramdisk/mqtt_ramdisk.db
-chmod -R 777 /mnt/ramdisk
-echo "Directory /mnt/ramdisk created."
+#rm -rf /mnt/ramdisk
+#mkdir /mnt/ramdisk
+#mount -osize=30m tmpfs /mnt/ramdisk -t tmpfs
+#chmod 777 /mnt/ramdisk
+#sqlite3 /mnt/ramdisk/mqtt_ramdisk.db "VACUUM;PRAGMA auto_vacuum = 1;"
+#chown birdofprey:birdofprey /mnt/ramdisk/mqtt_ramdisk.db
+#chmod -R 777 /mnt/ramdisk
+#echo "Directory /mnt/ramdisk created."
+
+function fail {
+    sleep 2
+    exit 1
+}
+python -u /opt/raspi-sensor-logger/ram_logger/ram_logger.py || fail
