39fe5d2d55258cfb1d5c7bcb4cebb5482af34935
Jitesh Pramodray <jiteshprm@gmail.com>
2021-05-14 00:37:19 +0100

    Update update delete with dedicated outdoor_update

--- esp_update_delete/esp_update_delete.sh
+++ esp_update_delete/esp_update_delete.sh
@@ -5,8 +5,8 @@ echo "Running:"
 while true
 do
   mosquitto_sub -h 127.0.0.1 -v -C 1 -t sensors/outdoor_update
-  echo Sleep 10s
-  sleep 10
+  echo Sleep 30s
+  sleep 30
   rm -rf /home/birdofprey/esp8266/OutdoorWeatherStation.ino.generic.bin
   echo Result: $?
 done
